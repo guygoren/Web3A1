d7165f893ca59174421ad9dee6e7bf751180b32b
guygoren <ggore337@mtroyal.ca>
2024-02-24 00:53:38 -0700

    Omg all done... except the readme

--- Guy Files/calls.js
+++ Guy Files/calls.js
@@ -6,7 +6,10 @@ const supaUrl = 'https://cyewshaaromzmbbsnszi.supabase.co';
 const supaAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5ZXdzaGFhcm9tem1iYnNuc3ppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgyNDY5OTQsImV4cCI6MjAyMzgyMjk5NH0.zruZ3mwscJDj8iPIHSCBPCNbg5QBCCyTKYvNHv_VBbU'; 
  
 const supabase = supa.createClient(supaUrl, supaAnonKey);
-app.get('/f1/status', async (req, res) => { 
+function jsonMsg(res, message) {
+  res.json({ message: message });
+}
+app.get('/api/status', async (req, res) => { 
     const {data, error} = await supabase 
     .from('status') 
     .select(); 
@@ -14,122 +17,195 @@ app.get('/f1/status', async (req, res) => {
    }); 
 app.listen(8080, () => { 
     console.log('listening on port 8080'); 
-    console.log('http://localhost:8080/f1/status'); 
+    console.log('http://localhost:8080/api/status'); 
    });
 //1
-   app.get('/f1/seasons', async (req, res) => { 
+   app.get('/api/seasons', async (req, res) => { 
     const {data, error} = await supabase 
     .from('seasons') 
     .select(); 
+    if(error){
+      jsonMsg(res, "error reading from the seasons table")
+    }
     res.send(data); 
    });
 //2
-app.get('/f1/circuits', async (req, res) => { 
+app.get('/api/circuits', async (req, res) => { 
     const {data, error} = await supabase 
     .from('circuits') 
     .select(); 
+    if(error){
+      jsonMsg(res, "error reading from the circuits table")
+    }
     res.send(data); 
    });
 //3
-app.get('/f1/circuits/:ref', async (req, res) => { 
+
+app.get('/api/circuits/:ref', async (req, res) => { 
     const {data, error} = await supabase 
     .from('circuits') 
     .select()
     .eq('circuitRef', req.params.ref);
+    if(error){
+      jsonMsg(res, "error reading from the circuits table")
+    }
+    if(!data.length){
+      jsonMsg(res, "Please check your ref as no records match")
+    }
     res.send(data); 
    });
 //4
-app.get('/f1/circuits/season/:year', async (req, res) => { 
+app.get('/api/circuits/season/:year', async (req, res) => { 
     const {data, error} = await supabase 
     .from('circuits') 
     .select()
     .eq('season', req.params.year);
+    if(error){
+      jsonMsg(res, "error reading from the database")
+    }
+    if(!data.length()){
+      jsonMsg(res, "Please check your year as no records match")
+    }
     res.send(data); 
    });
 //5
-app.get('/f1/constructors', async (req, res) => { 
+app.get('/api/constructors', async (req, res) => { 
     const {data, error} = await supabase 
     .from('constructors') 
     .select(); 
+    if(error){
+      jsonMsg(res, "error reading from the constructors table")
+    }
     res.send(data); 
    });
 //6
-app.get('/f1/constructors/:ref', async (req, res) => { 
+app.get('/api/constructors/:ref', async (req, res) => { 
     const {data, error} = await supabase 
     .from('constructors') 
     .select()
     .eq('constructorRef', req.params.ref);
+    if(error){
+      jsonMsg(res, "error reading from the constructors table")
+    }
+    if(!data.length()){
+      jsonMsg(res, "Please check your ref as no records match")
+    }
     res.send(data); 
    });
 //7
-app.get('/f1/drivers', async (req, res) => { 
+app.get('/api/drivers', async (req, res) => { 
     const {data, error} = await supabase 
     .from('drivers') 
     .select(); 
+    if(error){
+      jsonMsg(res, "error reading from the drivers table")
+    }
     res.send(data); 
    });
 //8
-app.get('/f1/drivers/:ref', async (req, res) => { 
+app.get('/api/drivers/:ref', async (req, res) => { 
     const {data, error} = await supabase 
     .from('drivers') 
     .select()
     .eq('driverRef', req.params.ref);
+    if(error){
+      jsonMsg(res, "error reading from the drivers table")
+    }
+    if(!data.length()){
+      jsonMsg(res, "Please check your ref as no records match")
+    }
     res.send(data); 
    });
 //9
-app.get('/f1/drivers/search/:beg', async (req, res) => { 
+app.get('/api/drivers/search/:beg', async (req, res) => { 
     const {data, error} = await supabase 
     .from('drivers') 
     .select() 
     .ilike('surname', req.params.beg+"%")
+    if(error){
+      jsonMsg(res, "error reading from the drivers table")
+    }
+    if(!data.length()){
+      jsonMsg(res, "Please check your surname as no records match")
+    }
     res.send(data); 
    });
 //10
-app.get('/f1/drivers/race/:raceid', async (req, res) => { 
+app.get('/api/drivers/race/:raceid', async (req, res) => { 
     const {data, error} = await supabase 
     .from('driverStanding') 
     .select('drivers(*)')
     .eq('raceId', req.params.raceid);
+    if(error){
+      jsonMsg(res, "error reading from the driverStanding table")
+    }
+    if(!data.length()){
+      jsonMsg(res, "Please check your raceId as no records match")
+    }
     res.send(data); 
    });
 //11
-app.get('/f1/races/:raceName', async (req, res) => { 
+app.get('/api/races/:raceName', async (req, res) => { 
     const {data, error} = await supabase 
     .from('races') 
     .select()
     .eq('raceId', req.params.raceName);
+    if(error){
+      jsonMsg(res, "error reading from the races table")
+    }
+    if(!data.length()){
+      jsonMsg(res, "Please check your race name as no records match")
+    }
     res.send(data); 
    });
 //12
-app.get('/f1/races/season/:year', async (req, res) => { 
+app.get('/api/races/season/:year', async (req, res) => { 
    const {data, error} = await supabase 
    .from('races') 
    .select()
    .eq('year', req.params.year)
    .order('round', { ascending: true }); 
+   if(error){
+      jsonMsg(res, "error reading from the races table")
+    }
+    if(!data.length()){
+      jsonMsg(res, "Please check your year as no records match")
+    }
    res.send(data); 
   });
 //13
-app.get('/f1/races/seasons/:year/:round', async (req, res) => { 
+app.get('/api/races/seasons/:year/:round', async (req, res) => { 
    const {data, error} = await supabase 
    .from('races') 
    .select()
    .match({year: req.params.year, round: req.params.round});
+   if(error){
+      jsonMsg(res, "error reading from the races table")
+    }
+    if(!data.length()){
+      jsonMsg(res, "Please check your year & round as no records match")
+    }
    res.send(data); 
   });
 //14
-app.get('/f1/races/circuits/:ref', async (req, res) => { 
+app.get('/api/races/circuits/:ref', async (req, res) => { 
    const {data, error} = await supabase 
    .from('races') 
    .select(`
    year, name, circuits !inner (name, location)
    `)
    .eq('circuits.circuitRef', req.params.ref)
-   .order('year', { ascending: true });  
+   .order('year', { ascending: true });
+   if(error){
+      jsonMsg(res, "error reading from the races table")
+    }
+    if(!data.length()){
+      jsonMsg(res, "Please check your ref as no records match")
+    }
    res.send(data); 
   });
 //16
-app.get('/f1/races/circuits/:ref/season/:first/:last', async (req, res) => { 
+app.get('/api/races/circuits/:ref/season/:first/:last', async (req, res) => { 
    const {data, error} = await supabase 
    .from('races') 
    .select(`*, circuits !inner ()
@@ -138,10 +214,19 @@ app.get('/f1/races/circuits/:ref/season/:first/:last', async (req, res) => {
    .lte('year', req.params.last) 
    .eq('circuits.circuitRef', req.params.ref)
    .order('year', { ascending: true }); 
+   if(error){
+      jsonMsg(res, "error reading from the races table")
+    }
+   if(req.params.last<req.params.first){
+     jsonMsg(res,"Your last year cannot precede your first") 
+   }
+    if(!data.length()){
+      jsonMsg(res, "Please check your ref & years as no records match")
+    }
    res.send(data); 
 });
 //17
-app.get('/f1/results/:id', async (req, res) => { 
+app.get('/api/results/:id', async (req, res) => { 
    const {data, error} = await supabase 
    .from('results') 
    .select(`
@@ -149,19 +234,31 @@ app.get('/f1/results/:id', async (req, res) => {
    `)
    .eq('raceId', req.params.id)
    .order('grid', { ascending: true });  
+   if(error){
+      jsonMsg(res, "error reading from the results table")
+    }
+    if(!data.length()){
+      jsonMsg(res, "Please check your id as no records match")
+    }
    res.send(data); 
   });
 //39
-app.get('/f1/results/driver/:ref', async (req, res) => { 
+app.get('/api/results/driver/:ref', async (req, res) => { 
    const {data, error} = await supabase 
    .from('results') 
    .select(`
    *, drivers!inner()`)
    .eq('drivers.driverRef', req.params.ref)
+   if(error){
+      jsonMsg(res, "error reading from the results table")
+    }
+    if(!data.length()){
+      jsonMsg(res, "Please check your ref as no records match")
+    }
    res.send(data); 
   });
 //40!!!!!
-// app.get('/f1/results/driver/:ref/seasons/:first/:last', async (req, res) => { 
+// app.get('/api/results/driver/:ref/seasons/:first/:last', async (req, res) => { 
 //    const {data, error} = await supabase 
 //   //  .from('results') 
 //   //    .select(`
@@ -173,36 +270,57 @@ app.get('/f1/results/driver/:ref', async (req, res) => {
 //   // .order('races.year', { ascending: true }); 
 //    res.send(data); 
 // });
-app.get('/f1/results/driver/:ref/seasons/:start/:end', async (req, res) => {
+app.get('/api/results/driver/:ref/seasons/:start/:end', async (req, res) => {
     const { data, error } = await supabase
         .from('results')
         .select('drivers!inner(forename,surname), races!inner(name,year), grid')
         .eq('drivers.driverRef', req.params.ref)
         .gte('races.year', req.params.start)
         .lte('races.year', req.params.end)
+        if(error){
+         jsonMsg(res, "error reading from the results table")
+       }
+      if(req.params.last<req.params.first){
+        jsonMsg(res,"Your last year cannot precede your first") 
+      }
+       if(!data.length()){
+         jsonMsg(res, "Please check your ref & years as no records match")
+       }
         res.send(data);
 });
 
 //41
-app.get('/f1/qualifying/:raceId/', async (req, res) => { 
+app.get('/api/qualifying/:raceId/', async (req, res) => { 
    const {data, error} = await supabase 
    .from('qualifying') 
    .select()
    .eq('raceId', req.params.raceId)
    .order('position', { ascending: true });
+   if(error){
+      jsonMsg(res, "error reading from the qualifying table")
+    }
+    if(!data.length()){
+      jsonMsg(res, "Please check your raceId as no records match")
+    }
    res.send(data); 
   });
 //42 
-app.get('/f1/standings/:raceId/drivers', async (req, res) => { 
+app.get('/api/standings/:raceId/drivers', async (req, res) => { 
    const {data, error} = await supabase 
    .from('driverStanding') 
    .select('drivers!inner(driverRef,code,forename,surname), races!inner(name), position')
    .eq('raceId', req.params.raceId)
    .order('position', { ascending: true });
+   if(error){
+      jsonMsg(res, "error reading from the driverStanding table")
+    }
+    if(!data.length()){
+      jsonMsg(res, "Please check your raceId as no records match")
+    }
    res.send(data); 
   });
 //43
-app.get('/f1/standings/:raceId/constructors', async (req, res) => { 
+app.get('/api/standings/:raceId/constructors', async (req, res) => { 
    const {data, error} = await supabase 
    .from('constructorStandings') 
    .select(`
@@ -210,5 +328,11 @@ app.get('/f1/standings/:raceId/constructors', async (req, res) => {
    `)
    .eq('raceId', req.params.raceId)
    .order('position', { ascending: true });
+   if(error){
+      jsonMsg(res, "error reading from the constructorStandings table")
+    }
+    if(!data.length()){
+      jsonMsg(res, "Please check your raceId as no records match")
+    }
    res.send(data); 
   });
